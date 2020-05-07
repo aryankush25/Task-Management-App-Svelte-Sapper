be387a5174e15e93cddd1a7f2509f4419f687f39
Aryan Agarwal <aryankush025@gmail.com>
2020-05-07 17:40:29 +0530

    auth added to layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -3,19 +3,24 @@
   import { unProtectedRoutes } from "../utils/helper.js";
   import { isTokensPresentLocalStorage } from "../services/utils/helper.js";
   import { goto } from "@sapper/app";
+  import api from "../services";
 
   export let segment;
 
   $: path = segment ? `/${segment}` : "/";
 
   const fetchAuthGuard = async route => {
-    const isLoggedIn = isTokensPresentLocalStorage();
-    const isProtectedRouted = !R.includes(route, unProtectedRoutes);
+    try {
+      const isLoggedIn = isTokensPresentLocalStorage();
+      const isProtectedRouted = !R.includes(route, unProtectedRoutes);
 
-    if (isLoggedIn && !isProtectedRouted) {
-      await goto("/");
-    } else if (!isLoggedIn && isProtectedRouted) {
-      await goto("/login");
+      if (isLoggedIn && !isProtectedRouted) {
+        await goto("/");
+      } else if (!isLoggedIn && isProtectedRouted) {
+        await goto("/login");
+      }
+    } catch (error) {
+      console.log("Error", error);
     }
   };
 
